f7da33d6cd5bfd83a8b1ad3c84f0e1edb45725bd
Dawit Girma <realdavis7779@gmail.com>
2024-02-07 14:42:40 +0300

    Dawit-Login-First

---
+++ js/all.js
@@ -0,0 +1,29 @@
+const themeToggle = document.getElementById("themeToggle");
+
+themeToggle.addEventListener("click", () => {
+  const body = document.body;
+  body.classList.toggle("dark");
+  const icon = themeToggle.querySelector("i");
+
+  if (body.classList.contains("dark")) {
+    icon.classList.remove("bx-moon");
+    icon.classList.add("bx-sun");
+    themeToggle.title = "Light Mode";
+  } else {
+    icon.classList.remove("bx-sun");
+    icon.classList.add("bx-moon");
+    themeToggle.title = "Dark Mode";
+  }
+});
+
+function togglePasswordVisibility() {
+  var passwordInput = document.getElementById("password");
+  var checkbox = document.getElementById("chk");
+
+  // Toggle password visibility based on checkbox state
+  if (checkbox.checked) {
+    passwordInput.type = "text";
+  } else {
+    passwordInput.type = "password";
+  }
+}
