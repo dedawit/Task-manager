f99de9e8a05776b6ce8781b9928147e766150656
henok-et <kindehenok@gmail.com>
2024-02-16 13:19:39 +0300

    Dynamic Fotter for both index and admin

---
+++ js/loadFooter.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const footerContainer = document.getElementById('footerContainer');
+
+    // Fetch the footer content
+    fetch('assets/footer.html')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Network response was not ok: ${response.statusText}`);
+            }
+            return response.text();
+        })
+        .then(html => {
+            
+            footerContainer.innerHTML = html;
+        })
+        .catch(error => {
+            console.error('Error loading footer:', error);
+        });
+});
